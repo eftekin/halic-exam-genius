7f3a1172e09fbe33c00a63a82315436ca130c9f8
Mustafa Eftekin <75522456+eftekin@users.noreply.github.com>
2024-06-29 21:04:38 +0300

    feat(utils.py): add function format_date to standardize date strings feat(utils.py): refactor date handling for exam date in different formats feat(utils.py): refactor time handling for exam time in different formats feat(utils.py): update creation of English and Turkish result DataFrames for consistency

--- utils.py
+++ utils.py
@@ -3,18 +3,29 @@ import datetime
 
 import pandas as pd
 import plotly.figure_factory as ff
+import requests
 from unidecode import unidecode
 
+
+# Define the function to format date strings
+def format_date(date_str):
+    try:
+        date_obj = datetime.datetime.strptime(date_str.split(" ")[0], "%Y-%m-%d")
+    except ValueError:
+        try:
+            date_obj = datetime.datetime.strptime(date_str.split(" ")[0], "%d.%m.%Y")
+        except ValueError:
+            return "Invalid date format"
+
+    formatted_date = date_obj.strftime("%d/%m/%Y")
+    return formatted_date
+
+
 # Read the Excel file into a DataFrame
+url = "https://halic.edu.tr/tr/s-duyurular/Documents/2024/06/10/2023-2024-bahar-donemi-final-sinavlari-tum-liste.xlsx"
+midterm_xls = requests.get(url)
 
-# import requests
-# url = "https://halic.edu.tr/tr/s-duyurular/Documents/2024/01/02/2023-2024-guz-donemi-final-sinav-programi-all-list.xlsx"
-# midterm_xls = requests.get(url)
-
-# df = pd.read_excel(midterm_xls.content)
-
-df = pd.read_excel("2023-2024-bahar-donemi-final-sinavlari-tum-liste.xlsx")
-
+df = pd.read_excel(midterm_xls.content)
 # Define column names for clarity
 exam_date_column = "SINAV GÜNÜ"
 exam_time_column = "BAŞLANGIÇ SAATİ"
@@ -80,14 +91,20 @@ def tr_getExamDate(course_code):
         0
     ]
     week_day = date.split(" ")[1]
-    date_full = datetime.datetime.strptime(date.split(" ")[0], "%Y-%m-%d").strftime(
-        "%d/%m/%Y"
-    )
+    date_full = format_date(date)
     date = date_full + " " + week_day
     time = df[df[course_code_and_name_column] == course_code][exam_time_column].values[
         0
     ]
-    return str(date) + " " + str(time)
+    if isinstance(time, str):
+        try:
+            time_obj = datetime.datetime.strptime(time, "%H:%M:%S").time()
+        except ValueError:
+            time_obj = datetime.datetime.strptime(time, "%H:%M").time()
+    else:
+        time_obj = time
+    time_str = time_obj.strftime("%H:%M")
+    return str(date) + " " + time_str
 
 
 # Retrieve and format the exam date and time in English
@@ -95,15 +112,24 @@ def en_getExamDate(course_code):
     date = df[df[course_code_and_name_column] == course_code][exam_date_column].values[
         0
     ]
-    date_en = datetime.datetime.strptime(date.split(" ")[0], "%Y-%m-%d").strftime(
-        "%d/%m/%Y %A"
+    date_formatted = format_date(date.split(" ")[0])
+    date_en = (
+        date_formatted
+        + " "
+        + datetime.datetime.strptime(date_formatted, "%d/%m/%Y").strftime("%A")
     )
-    date = date_en
-
     time = df[df[course_code_and_name_column] == course_code][exam_time_column].values[
         0
     ]
-    return date + " " + time
+    if isinstance(time, str):
+        try:
+            time_obj = datetime.datetime.strptime(time, "%H:%M:%S").time()
+        except ValueError:
+            time_obj = datetime.datetime.strptime(time, "%H:%M").time()
+    else:
+        time_obj = time
+    time_str = time_obj.strftime("%H:%M")
+    return date_en + " " + time_str
 
 
 # Retrieve the course name
@@ -128,8 +154,7 @@ def getClassroom(course_code):
 # Create a DataFrame in English
 def en_createResultDf(course_list):
     result_df_en = pd.DataFrame(
-        [],
-        columns=["Course Name", "Exam Date", "Classroom Codes"],
+        [], columns=["Course Name", "Exam Date", "Classroom Codes"]
     )
     for course in course_list:
         list_row = [getCourseName(course), en_getExamDate(course), getClassroom(course)]
@@ -140,10 +165,7 @@ def en_createResultDf(course_list):
 
 # Create a DataFrame in Turkish
 def tr_createResultDf(course_list):
-    result_df_tr = pd.DataFrame(
-        [],
-        columns=["Ders Adı", "Sınav Tarihi", "Sınıf"],
-    )
+    result_df_tr = pd.DataFrame([], columns=["Ders Adı", "Sınav Tarihi", "Sınıf"])
     for course in course_list:
         list_row = [getCourseName(course), tr_getExamDate(course), getClassroom(course)]
         result_df_tr.loc[len(result_df_tr)] = list_row
