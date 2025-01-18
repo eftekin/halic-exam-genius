5c5dd9038b3e76930ac093e3c82172986358f952
eftekin <m_eftekin@hotmail.com>
2025-01-18 20:46:37 +0300

    fix: update last update timestamp in application configuration

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def main():
     Handles grade calculation and exam date display.
     """
     # Application configuration
-    last_update = "17.01.2025 17:15"  # Last update date and time
+    last_update = "18.01.2025 20:40"  # Last update date and time
     st.title("Exam Genius📚")
     language_on = st.toggle("🇺🇸 EN", key="language_toggle", value=False)
 
